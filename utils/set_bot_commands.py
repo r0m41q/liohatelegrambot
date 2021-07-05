aaa219b4451d9b434c6d586b0db1914b673b5aba
unknown <baron.balanyuk@gmail.com>
2021-07-05 16:08:20 +0300

    add new command to bot menu

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -8,6 +8,7 @@ async def set_default_commands(dp):
             types.BotCommand("pidor", "To choose one to rule them all"),
             types.BotCommand("fact", "It is what it is"),
             types.BotCommand("meme", "Facepalm"),
+            types.BotCommand("stats", "Get stats"),
             types.BotCommand("help", "Get info"),
             types.BotCommand("statham", "To learn from the wisest of all men"),
         ]
