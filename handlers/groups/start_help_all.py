812773f71ccc859622e5c981c77dc9763d6a7453
Alex <82285840+nonik000@users.noreply.github.com>
2021-08-31 08:24:19 +0300

    Update start_help_all.py

--- handlers/groups/start_help_all.py
+++ handlers/groups/start_help_all.py
@@ -7,12 +7,12 @@ from loader import dp
 from data.data_for_bot import Help, Hello
 
 
-@dp.message_handler(CommandHelp())
+@dp.message_handler(commands=['help'])
 async def bot_help(message: types.Message):
     await message.answer(Help)
 
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(commands=['start'])
 async def bot_start(message: types.Message):
     await message.answer(Hello)
 
