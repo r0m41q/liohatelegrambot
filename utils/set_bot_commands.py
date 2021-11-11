aff3c28e84ec839d324fb707f1db945e9571e5f3
nonik000 <baron.balanyuk@gmail.com>
2021-11-11 09:57:51 +0200

    upgraded translit dictionary

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,13 +4,12 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("all", "To call upon the khuilyky"),
             types.BotCommand("pidor", "To choose one to rule them all"),
             types.BotCommand("fact", "It is what it is"),
             types.BotCommand("meme", "Facepalm"),
             types.BotCommand("stats", "Get stats"),
-            types.BotCommand("help", "Get info"),
+            types.BotCommand("anon_msg", "Send anonymous message"),
             types.BotCommand("statham", "To learn from the wisest of all men"),
-            types.BotCommand("anon_msg", "send anonymous message"),
+            types.BotCommand("all", "To call upon the khuilyky"),
         ]
     )
