0cc2efc22d354dd2b0faa53d89796b91e2bc30d9
@nonik000 <baron.balanyuk@gmail.com>
2022-03-21 16:30:36 +0200

    set bot commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,12 +4,13 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("pidor", "To choose one to rule them all"),
-            types.BotCommand("fact", "It is what it is"),
-            types.BotCommand("meme", "Facepalm"),
+            types.BotCommand("bundle", "Why click more?"),
             types.BotCommand("stats", "Get stats"),
             types.BotCommand("anon_msg", "Send anonymous message"),
             types.BotCommand("statham", "To learn from the wisest of all men"),
             types.BotCommand("all", "To call upon the khuilyky"),
+            types.BotCommand("pidor", "To choose one to rule them all"),
+            types.BotCommand("fact", "It is what it is"),
+            types.BotCommand("meme", "Facepalm"),
         ]
     )
